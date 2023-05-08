a6024f9d076574df84db22587b207328e3cb0e35
Ronald <rnor3536@uni.sydney.edu.au>
2023-05-08 16:40:18 +1000

    Initial Commit

---
+++ EurovisionAdminApp/EurovisionAdminAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EurovisionAdminAppApp.swift
+//  EurovisionAdminApp
+//
+//  Created by Ronald Noronha on 8/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct EurovisionAdminAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
